71cd19b68f9565255d02b2b5d977038baa832ed0
Vagrant User <vagrant@multicloud>
2021-11-06 11:48:12 -0300

    App Engine Provision

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,22 +1,4 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "firstvm" {
-  name         = "helloworld"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-c"
-
-  # Defini a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20210927"
-    }
-  }
-
-  # Habilita rede para a VM com um IP público
-  network_interface {
-    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
-
-    access_config {
-    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
-    }
-  }
+resource "google_app_engine_application" "app" {
+  project     = "lab-devops-cloud-paulomarques"
+  location_id = "us-central"
 }
